c9ec1cd42adf8890b546caf9a30f97a5e2ffa0e7
Chamikara Samarasekara <ChamikaraS@zone24x7.com>
2018-10-18 09:06:12 +0530

    Clear Project

--- chall/README.md
+++
@@ -1,4 +0,0 @@
-chall
-=====
-
-A Symfony project created on September 20, 2018, 9:53 am.
--- chall/src/StudentInfoBundle/StudentInfoBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace StudentInfoBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class StudentInfoBundle extends Bundle
-{
-}
